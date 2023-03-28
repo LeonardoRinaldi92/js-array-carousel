4b0800654a187079d47df6347994acb1446ce24e
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-28 17:25:42 +0200

    Struttura 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,49 @@ let photos = [
     './assets/img/05.webp' 
 ]
 
-console.log (photos)
 
 for (x=0; x<=photos.length -1; x++) {
-
-    
+  
     if (x==0) {
         document.getElementById('carousel').innerHTML += `<img src=" ${photos[x]}" class="foto prima selected ">`;
     } else if (x==4) {
         document.getElementById('carousel').innerHTML += `<img src=" ${photos[x]}" class="foto ultima">`;
     } else {
         document.getElementById('carousel').innerHTML += `<img src=" ${photos[x]}" class="foto">`;
-    }
-    
+    } 
 }
 
+const next = document.querySelector('.next')
+const prec = document.querySelector('.prec')
+
+
+next.addEventListener( 'click', function(){
+
+    let SelectedPhoto = document.querySelector('img.selected')
+    let photoToSwitch = SelectedPhoto.nextElementSibling
+
+    if( SelectedPhoto.classList.contains('ultima') ){
+        photoToSwitch = document.querySelector(' .prima')
+    }
+
+    SelectedPhoto.classList.remove('selected')
+    photoToSwitch.classList.add('selected')
+
+})
+
+prec.addEventListener( 'click', function(){
+
+    let SelectedPhoto = document.querySelector('img.selected')
+    let photoToSwitch = SelectedPhoto.previousElementSibling
+
+
+    if( SelectedPhoto.classList.contains('prima') ){
+        photoToSwitch = document.querySelector('.ultima')
+    }
+
+    SelectedPhoto.classList.remove('selected')
+    photoToSwitch.classList.add('selected')
+
+})
     
 
